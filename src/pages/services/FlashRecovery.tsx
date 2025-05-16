62913fa1070bbf8b0f8860852249f24e816c8b0d
WizTizzle <info@wiztech.zip>
2025-05-16 11:09:10 -0400

    Adjust flash drive image position

--- src/pages/services/FlashRecovery.tsx
+++ src/pages/services/FlashRecovery.tsx
@@ -16,7 +16,7 @@ export function FlashRecovery() {
           <img
             src="/images/FLASH.jpg"
             alt="Flash drive recovery"
-            className="w-full rounded-lg object-cover h-64 object-[center_-2rem]"
+            className="w-full rounded-lg object-cover h-64 object-top"
           />
           <img
             src="/images/USB.jpg"
