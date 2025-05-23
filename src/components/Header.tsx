0e721b748d42e538f7cf7b0a0c27bd06c902ce5a
WizTizzle <info@wiztech.zip>
2025-05-23 10:06:14 -0400

    Remove Logo and DATA RECOVERY Text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,22 +33,7 @@ export function Header() {
               transition={{ type: "spring", stiffness: 400 }}
             >
               <Link to="/" className="block">
-                <div 
-                  className="relative flex flex-col items-center"
-                  style={{
-                    transform: `rotate(0deg) translate(0px, 0px)`,
-                    marginLeft: '10%'
-                  }}
-                >
-                  <img 
-                    src="/images/Final logo WIZTECH.png"
-                    alt="WizTech Logo"
-                    className="h-[104px] w-auto object-contain transition-all duration-300"
-                  />
-                  <div className="text-xl font-display tracking-[0.2em] text-transparent bg-clip-text bg-gradient-to-r from-primary-300 via-primary-400 to-primary-300 animate-shine [background-size:200%_auto] -mt-1">
-                    DATA RECOVERY
-                  </div>
-                </div>
+                {/* Logo removed to start fresh */}
               </Link>
             </motion.div>
           </div>
